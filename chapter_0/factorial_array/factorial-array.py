b6a07402b9a3f385510ca2fce9c7123d730534a8
nbabitsk-crypto <nbabitsk@andrew.cmu.edu>
2026-09-01 16:48:41 -0400

    Complete FactorialArray in python

---
+++ chapter_0/factorial_array/factorial-array.py
@@ -0,0 +1,22 @@
+def factorial_array(n: int) -> list[int]:
+    """
+    Produces a list of all factorials from 0! to n!
+    
+    Parameters:
+    - n (int)
+    
+    Returns:
+    list(int): A list of length n+1 where the k-th element is k!
+    """
+
+    if n<0:
+            raise ValueError("Error: negative input given.")
+
+    fact = [0]*(n+1)
+
+    fact[0] = 1
+    # range through and set k! = k*(k-1)!
+    for k in range(1, n+1):
+         fact[k] = fact[k-1]*k
+
+    return fact
