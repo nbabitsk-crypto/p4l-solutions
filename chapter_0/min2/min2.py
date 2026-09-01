79d31e6f77b7d9a9448ccd5cdaff071753d9397e
nbabitsk-crypto <nbabitsk@andrew.cmu.edu>
2026-09-01 16:46:38 -0400

    Complete Min2 in python

---
+++ chapter_0/min2/min2.py
@@ -0,0 +1,15 @@
+def min_2(a: int, b: int) -> int:
+    """
+    Takes two integers and returns their minimum
+    
+    Parameters:
+    - a (int)
+    - b (int)
+
+    Returns:
+    int: minimum of a and b
+    """
+    if a < b:
+        return a  #  a is smaller
+    else:  #  b is greater than or equal to a
+        return b
