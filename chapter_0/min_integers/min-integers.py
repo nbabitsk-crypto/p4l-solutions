06155de5af77b1c391d25160e8c150829d8c1dd5
nbabitsk-crypto <nbabitsk@andrew.cmu.edu>
2026-09-01 16:48:55 -0400

    Complete MinIntegers in python

---
+++ chapter_0/min_integers/min-integers.py
@@ -0,0 +1,12 @@
+def min_integers(*numbers: int) -> int:
+     # numbers is a tuple
+    if len(numbers) == 0:
+         raise ValueError("No values given to function.")
+
+    m = numbers[0]
+
+    for val in numbers:
+         if val < m:
+              m = val
+
+    return m
