60975ec90023c21d5a9891672b2f868900f52263
nbabitsk-crypto <nbabitsk@andrew.cmu.edu>
2026-09-01 16:46:56 -0400

    Complete SameSign in python

---
+++ chapter_0/same_sign/same-sign.py
@@ -0,0 +1,16 @@
+def same_sign(x: int, y: int) -> bool:
+    """
+    Returns True if two input integers have the same sign and False otherwise.
+    
+    Parameters:
+    - x (int)
+    - y (int)
+    
+    Returns:
+    bool: True if x and y have the same sign and False otherwise (zero has the same sign as all integers)
+    """
+    #  three cases:
+    #  1. both positive (x*y >= 0, True)
+    #  2. both negative (x*y >= 0, True)
+    #  3. opposite signs (x*y <= 0, False)
+    return(x*y >= 0)
