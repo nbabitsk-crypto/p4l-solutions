d760ca0b0df0a6fbb593c09afd0336a279fa2188
nbabitsk-crypto <nbabitsk@andrew.cmu.edu>
2026-09-01 16:46:47 -0400

    Complete WhichIsGreater in python

---
+++ chapter_0/which_is_greater/which-is-greater.py
@@ -0,0 +1,16 @@
+def which_is_greater(x:int, y: int) -> int:
+    """
+    Takes two integers as input and returns 1 if the first one is larger, 0 if they're equal, and -1 if the second one is larger.
+    Parameters:
+    - x (int)
+    - y (int)
+    
+    Returns:
+    int: 1 if x>y, -1 if x<y, 0 if x=y
+    """
+    if x == y:
+        return 0
+    elif x > y:
+        return 1
+    else:  # we know here that x < y
+        return -1
