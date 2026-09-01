f1328f876f9c21f3a22f693c5509c8a0f14a9aa8
nbabitsk-crypto <nbabitsk@andrew.cmu.edu>
2026-09-01 16:46:18 -0400

    Complete SumTwoInts in python

---
+++ chapter_0/sum_two_ints/sum-two-ints.py
@@ -0,0 +1,13 @@
+#
+def sum_two_ints(a:int, b:int) -> int:
+    """
+    Returns the sum of two input integers.
+
+    Parameters:
+    - a (int)
+    - b (int)
+
+    Returns:
+    int: a + b
+    """
+    return a+b  #  this is the output of the function
