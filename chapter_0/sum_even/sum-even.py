fac8f50bd6dc07fedabe7dece06156fd0d74225c
nbabitsk-crypto <nbabitsk@andrew.cmu.edu>
2026-09-01 16:48:30 -0400

    Complete SumEven in python

---
+++ chapter_0/sum_even/sum-even.py
@@ -0,0 +1,18 @@
+def sum_even(k: int) -> int:
+    """
+    Sums all the even positive integers up to, and possibly including, k.
+    
+    Parameters:
+    - k: int
+    
+    Returns:
+    - int: sum of the even positive integers up to k.
+    """
+    if k < 0:
+        raise ValueError("Error: Negative k given to function.")
+
+    s = 0
+
+    for i in range(2, k+1, 2):
+        s += i
+    return s
