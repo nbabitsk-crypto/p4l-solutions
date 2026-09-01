b7a2a8f7020798a83c976178a9e95219e9978c04
nbabitsk-crypto <nbabitsk@andrew.cmu.edu>
2026-09-01 16:47:34 -0400

    Complete PositiveDifference in python

---
+++ chapter_0/positive_difference/positive-difference.py
@@ -0,0 +1,15 @@
+def positive_difference(a: int, b: int) -> int:
+    """
+    Parameters:
+    - a (int)
+    - b (int)
+
+    Returns:
+    - the absolute value of the difference between a and b
+    """
+    if a-b > 0:
+        return a-b
+    else:
+        return b-a
+
+        # I must have missed this part of the code along so I came up with this code on my own, but I will ask whether it's the most efficient code I could write during recitation!
