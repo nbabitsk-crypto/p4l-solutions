c4ee7a6f2f30d553179ccfe0fca3b1cae883fc3e
nbabitsk-crypto <nbabitsk@andrew.cmu.edu>
2026-09-01 16:48:48 -0400

    Complete MinIntegerArray in python

---
+++ chapter_0/min_integer_array/min-integer-array.py
@@ -0,0 +1,13 @@
+def  min_integer_array(a: list[int]) -> int:
+    if len(a) == 0:
+        raise ValueError("Error: empty list given to function.")
+
+    m = a[0]  # stores our minimum
+
+    for val in a:
+         # is the current value better than what I currently have?
+         if val < m:
+              # update m appropriately
+              m = val
+
+    return m
