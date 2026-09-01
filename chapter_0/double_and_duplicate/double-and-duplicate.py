12776ea5ef861a7352557dd4447730da5593e59d
nbabitsk-crypto <nbabitsk@andrew.cmu.edu>
2026-09-01 16:46:31 -0400

    Complete DoubleAndDuplicate in python

---
+++ chapter_0/double_and_duplicate/double-and-duplicate.py
@@ -0,0 +1,12 @@
+# functions can also return more than one value
+def double_and_duplicate(x: float) -> tuple[float, float]:
+    """
+    Double the input variable and return two copies of it.
+
+    Parameters:
+    - x (float)
+
+    Returns:
+    Two copies of 2*x
+    """
+    return 2*x, 2*x
