3e69cc0f67e31bc30a9e215de0336efc33eec448
nbabitsk-crypto <nbabitsk@andrew.cmu.edu>
2026-09-04 18:29:17 -0400

    Complete Reverse in python

---
+++ chapter_1/reverse/reverse.py
@@ -0,0 +1,20 @@
+def reverse(s: str) -> str:
+    """
+    Reverses the symbols in an input string
+
+    Parameters:
+    - s: str
+
+    Returns:
+    str: the reverse of s
+    """
+    characters = []
+    rev = ""
+
+    n = len(s)
+
+    for i in range (n):
+        characters.append(s[n-1-i])
+
+    # now we need to convert this list to a string
+    return "".join(characters)
