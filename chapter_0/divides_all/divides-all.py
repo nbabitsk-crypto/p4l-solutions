9df67d63f816e4652ceeee7bb19561f4a39d24d6
nbabitsk-crypto <nbabitsk@andrew.cmu.edu>
2026-09-09 15:36:47 -0400

    Complete DividesAll in python

---
+++ chapter_0/divides_all/divides-all.py
@@ -0,0 +1,17 @@
+# Insert your divides_all() function here, along with any subroutines that you need.
+def divides_all(a: list[int], d: int) -> bool:
+    """
+    Determine whether d divides every element of a.
+    Args:
+        a: A list of integers.
+        d: The candidate divisor.
+    Returns:
+        True if every element x in a satisfies x % d == 0.
+        False immediately if d == 0, since zero is not a divisor of any number.
+    """
+    if d == 0:
+        return False
+    for i in range(len(a)):
+        if a[i] % d != 0:
+            return False
+    return True
