65415a0d1b4dd60988201ed4706784ead5fc7db9
nbabitsk-crypto <nbabitsk@andrew.cmu.edu>
2026-09-11 16:30:26 -0400

    Complete MaxIntegerArray in python

---
+++ chapter_0/max_integer_array/max-integer-array.py
@@ -0,0 +1,14 @@
+# Insert your max_integer_array() function here, along with any subroutines that you need.
+def max_integer_array(lst: list[int]) -> int:
+    """
+    Return the maximum integer in a non-empty list.
+    Args:
+        lst: A non-empty list of integers.
+    Returns:
+        The largest integer in lst.
+    """
+    k = lst[0]  # k contains the current largest integer
+    for i in range(1, len(lst)):
+        if lst[i] > k:
+            k = lst[i]
+    return k
