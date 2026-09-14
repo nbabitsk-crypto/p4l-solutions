1b657a9dde2b0eabcb4078707197963be0c56f46
nbabitsk-crypto <nbabitsk@andrew.cmu.edu>
2026-09-14 19:29:37 -0400

    Complete RollDie in python

---
+++ chapter_2/roll_die/roll-die.py
@@ -0,0 +1,12 @@
+import random # this should be helpful!
+
+# Write your roll_die() function here along with any subroutines that you need.
+def roll_die() -> int:
+    """
+    Simulates the roll of a die.
+    Parameters:
+    - none
+    Returns:
+    - int: A pseudorandom integer between 1 and 6, inclusively.
+    """
+    return random.randrange(1,7)
