3552de1be3a59ff201b86a4e5f114f7b58f2b1b6
nbabitsk-crypto <nbabitsk@andrew.cmu.edu>
2026-09-14 19:37:37 -0400

    Complete SumDice in python

---
+++ chapter_2/sum_dice/sum-dice.py
@@ -0,0 +1,28 @@
+import math
+import random # this should be helpful!
+
+def roll_die() -> int:
+    """
+    Simulates the roll of a die.
+    Parameters:
+    - none
+    Returns:
+    - int: A pseudorandom integer between 1 and 6, inclusively.
+    """
+    return random.randrange(1,7)
+
+def sum_dice(num_dice: int) -> int:
+    """
+    Simulates the rolling of two dice.
+    Parameters:
+    - num_dice: int
+    Returns:
+    - int: A pseudorandom integer corresponding to simulating the roll of num_dice dice and returning their sum.
+    """
+    total = 0
+
+    # range over number of dice and add one roll's value to the total
+    for _ in range(num_dice):
+        total += roll_die()
+
+    return total
