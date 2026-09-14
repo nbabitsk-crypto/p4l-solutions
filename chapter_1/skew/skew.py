d2a4eda5f6b4faea7cdad4812fb0bd5384362541
nbabitsk-crypto <nbabitsk@andrew.cmu.edu>
2026-09-13 21:16:07 -0400

    Complete Skew in python

---
+++ chapter_1/skew/skew.py
@@ -0,0 +1,23 @@
+def skew(symbol: str) -> int:
+    """
+    Returns 1 if symbol is 'G'/'g', -1 if 'C'/'c', else 0.
+
+    Parameters:
+    - symbol (str): Single-character string.
+
+    Returns:
+    - int: Skew contribution for this symbol.
+
+    Raises:
+    - ValueError: If `symbol` is not length 1.
+    """
+    # TODO: Implement this function
+    skew: dict[str, int] = {
+        "A": 0,
+        "C": -1,
+        "G": 1,
+        "T": 0,
+    }
+    if symbol in skew:
+        return skew[symbol]
+    return 0
