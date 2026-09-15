5adcab3514bccb21b292a93056d73c73ca113e28
nbabitsk-crypto <nbabitsk@andrew.cmu.edu>
2026-09-15 18:40:15 -0400

    Complete 244400 in python

---
+++ 244400/244400.py
@@ -0,0 +1,22 @@
+def at_skew_array(genome: str) -> list[int]:
+    """
+    Compute the AT skew array of a genome string.
+
+    Parameters:
+        genome (str): A DNA string consisting of 'A', 'C', 'G', 'T' (case-insensitive).
+
+    Returns:
+        list[int]: The AT skew array, starting with 0, where each subsequent
+                   value is the previous value plus +1 for 'A', -1 for 'T',
+                   and 0 otherwise.
+    """
+    skew = [0]
+
+    for i in range(len(genome)):
+        if genome[i] == "A":
+            skew.append(skew[i]+1)
+        elif genome[i] == "T":
+            skew.append(skew[i] - 1)
+        else:
+            skew.append(skew[i] + 0)
+    return skew
